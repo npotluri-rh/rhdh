66efddfe436b52c9072de15613567bb523dd3629
npotluri-rh <114231130+npotluri-rh@users.noreply.github.com>
2025-06-24 14:57:17 -0500

    test with json output

--- e2e-tests/playwright.config.ts
+++ e2e-tests/playwright.config.ts
@@ -26,9 +26,7 @@ export default defineConfig({
   workers: 3,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ["html"],
-    ["list"],
-    ["junit", { outputFile: "junit-results.xml" }],
+    ['json', {  outputFile: 'test-e2e-results.json' }]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
